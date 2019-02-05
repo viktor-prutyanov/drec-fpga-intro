6d5358d0cde1aef6a695cc5bb2dcaeb57caa072f
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2019-02-05 17:30:10 +0300

    Add problem set #01
    Signed-off-by: Viktor Prutyanov <viktor.prutyanov@phystech.edu>

---
+++ problems/01_clock_div/testbench.v
@@ -0,0 +1,33 @@
+`timescale 1 ns / 100 ps
+
+/*
+*   Introduction to FPGA and Verilog
+*
+*   Viktor Prutyanov, 2019
+*
+*   Testbench for problem set #01
+*/
+
+module testbench();
+
+reg clk = 1'b0;
+
+always begin
+    #1 clk = ~clk;
+end
+
+/*
+*   Problem 2/3:
+*   Make an instance of your new module clk_div.
+*   Create approprite wires to test it.
+*/
+
+initial begin
+    $dumpvars;      /* Open for dump of signals */
+    /*
+    *   Problem 2/3:
+    *   Stop simulation after several cycles of your (divided) clock.
+    */
+end
+
+endmodule
