ddec0e3eac5a24c0fee69c99742669aa4471bdde
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2019-04-19 12:39:52 +0300

    Fix comment in problem set #08
    Signed-off-by: Viktor Prutyanov <viktor.prutyanov@phystech.edu>

--- problems/08_cpu_v2/samples/test_v2.s
+++ problems/08_cpu_v2/samples/test_v2.s
@@ -5,9 +5,9 @@
 _start:
     li      x11,    0x720
     li      x15,    0x722
-    add     x11,    x11,    x15 # x11 <- 0x42
+    add     x11,    x11,    x15 # x11 <- 0xe42
     li      x7,     0x10
-    sw      x11,    0x10(x7)    # [0x20] <- 0x42
+    sw      x11,    0x10(x7)    # [0x20] <- 0xe42
 
 _finish:
     nop
