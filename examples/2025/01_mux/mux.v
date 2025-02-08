04ca10073d265e8f3edbf79850d3e2eafcfc27dc
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2025-02-09 00:49:32 +0300

    Add lecture #2 example 01_mux

---
+++ examples/2025/01_mux/mux.v
@@ -0,0 +1,29 @@
+module mux #(
+    parameter WIDTH = 32
+)(
+    input  wire [WIDTH-1:0] i0, i1,
+    input  wire             sel,
+    output wire [WIDTH-1:0] out
+);
+
+always @(*) begin
+    case (sel)
+        1'b0: out = i0;
+        1'b1: out = i1;
+    endcase
+end
+
+/*
+assign out = sel ? i1 : i0;
+*/
+
+/*
+always @(*) begin
+    if (sel)
+        out = i1;
+    else
+        out = i0;
+end
+*/
+
+endmodule
