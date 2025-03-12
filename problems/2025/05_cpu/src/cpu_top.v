d88e6b61931cd7cafe7f95a06a6367e061fc00c3
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2025-03-12 17:03:47 +0300

    Update 05_cpu problem

---
+++ problems/2025/05_cpu/src/cpu_top.v
@@ -0,0 +1,89 @@
+`include "config.vh"
+
+module cpu_top(
+    input  wire clk,
+    input  wire rst_n,
+
+    output wire [29:0] o_mmio_addr,
+    output wire [31:0] o_mmio_data,
+    output wire  [3:0] o_mmio_mask,
+    output wire        o_mmio_wren,
+    input  wire [31:0] i_mmio_data
+);
+
+wire [29:0] core2imem_addr;
+wire [31:0] imem2core_data;
+
+wire [29:0] core2xbar_addr;
+wire [31:0] core2xbar_data;
+wire        core2xbar_wren;
+wire  [3:0] core2xbar_mask;
+wire [31:0] xbar2core_data;
+
+wire [29:0] xbar2dmem_addr;
+wire [31:0] xbar2dmem_data;
+wire        xbar2dmem_wren;
+wire  [3:0] xbar2dmem_mask;
+wire [31:0] dmem2xbar_data;
+
+imem #(
+    .ADDR_WIDTH     (`IMEM_ADDR_WIDTH),
+    .INIT_FILE      (`IMEM_FILE_TXT  ),
+    .DATA_WIDTH     (32              )
+)
+imem(
+    .i_addr         (core2imem_addr  ),
+    .o_data         (imem2core_data  )
+);
+
+dmem #(
+     .ADDR_WIDTH    (`DMEM_ADDR_WIDTH),
+     .DATA_WIDTH    (32              )
+)
+dmem(
+     .clk           (clk             ),
+     .i_addr        (xbar2dmem_addr  ),
+     .i_data        (xbar2dmem_data  ),
+     .i_we          (xbar2dmem_wren  ),
+     .i_mask        (xbar2dmem_mask  ),
+     .o_data        (dmem2xbar_data  )
+);
+
+mem_xbar #(
+    .DATA_START    (`XBAR_DATA_START),
+    .DATA_LIMIT    (`XBAR_DATA_LIMIT),
+    .MMIO_START    (`XBAR_MMIO_START),
+    .MMIO_LIMIT    (`XBAR_MMIO_LIMIT)
+)
+mem_xbar(
+    .i_addr        (core2xbar_addr  ),
+    .i_data        (core2xbar_data  ),
+    .i_wren        (core2xbar_wren  ),
+    .i_mask        (core2xbar_mask  ),
+    .o_data        (xbar2core_data  ),
+    .o_dmem_addr   (xbar2dmem_addr  ),
+    .o_dmem_data   (xbar2dmem_data  ),
+    .o_dmem_mask   (xbar2dmem_mask  ),
+    .o_dmem_wren   (xbar2dmem_wren  ),
+    .i_dmem_data   (dmem2xbar_data  ),
+    .o_mmio_addr   (o_mmio_addr     ),
+    .o_mmio_data   (o_mmio_data     ),
+    .o_mmio_wren   (o_mmio_wren     ),
+    .o_mmio_mask   (o_mmio_mask     ),
+    .i_mmio_data   (i_mmio_data     )
+);
+
+core core(
+    .clk           (clk             ),
+    .rst_n         (rst_n           ),
+    .i_instr_data  (imem2core_data  ),
+    .o_instr_addr  (core2imem_addr  ),
+    .o_mem_addr    (core2xbar_addr  ),
+    .o_mem_data    (core2xbar_data  ),
+    .o_mem_we      (core2xbar_wren  ),
+    .o_mem_mask    (core2xbar_mask  ),
+    .i_mem_data    (xbar2core_data  )
+);
+
+endmodule
+
---
+++ problems/2025/05_cpu/src/cpu_top_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns/1ps
+
+module cpu_top_tb;
+
+localparam PC_OFFSET = 32'h0;
+
+reg clk   = 1'b0;
+reg rst_n = 1'b0;
+
+always begin
+    #1 clk <= ~clk;
+end
+
+initial begin
+    @(posedge clk)
+    @(posedge clk)
+    rst_n <= 1'b1;
+end
+
+wire [29:0] o_mmio_addr;
+wire [31:0] o_mmio_data;
+wire  [3:0] o_mmio_mask;
+wire        o_mmio_wren;
+wire [31:0] i_mmio_data = 32'bX;
+
+wire [31:0] ip = PC_OFFSET + {cpu_top.core.pc, 2'b0};
+
+cpu_top cpu_top(
+    .clk            (clk        ),
+    .rst_n          (rst_n      ),
+    .o_mmio_addr    (o_mmio_addr),
+    .o_mmio_data    (o_mmio_data),
+    .o_mmio_mask    (o_mmio_mask),
+    .o_mmio_wren    (o_mmio_wren),
+    .i_mmio_data    (i_mmio_data)
+);
+
+always @(posedge clk) begin
+    if (o_mmio_wren)
+        $display("[%t] %m: mmio: [0x%h] <- %h (%b)", $realtime,
+                {o_mmio_addr, 2'b0}, o_mmio_data, o_mmio_mask);
+end
+
+initial begin
+    $dumpvars;
+    #1500 $finish;
+end
+
+endmodule
